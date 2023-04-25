fc5fb53d22948410107cb0605d47d9fd1fff0649
Wen Chang <wchang@cerberus.com>
2023-04-25 15:30:47 -0400

    first commit

--- app.js
+++ app.js
@@ -11,19 +11,26 @@ if (!process.env['S3_SECRET_ACCESS_KEY']) {
 	process.exit(1);
 }
 
-if (process.argv.length < 3)
+if (process.argv.length < 3 || !process.argv[2]) {
+	console.error(`file path not set`);
+	process.exit(1);
+}
 
 const filePath = process.argv[2];
-const s3_url = process.argv[3];
 
+if (process.argv.length < 4 || !process.argv[3]) {
+	console.error(`S3 url not set`);
+	process.exit(1);
+}
+
+const s3_url = process.argv[3];
 const parsedUrl = url.parse(s3_url);
 //console.log(parsedUrl);
 //process.exit(0);
 const Bucket = parsedUrl.hostname;
 const Key = parsedUrl.pathname.substr(1);
-
-console.log(`Bucket=${Bucket}`);
-console.log(`Key=${Key}`);
+// console.log(`Bucket=${Bucket}`);
+//console.log(`Key=${Key}`);
 const s3 = new AWS.S3({
     accessKeyId: process.env['S3_ACCESS_KEY_ID'],
     secretAccessKey: process.env['S3_SECRET_ACCESS_KEY']
